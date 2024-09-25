d8184c6c5b1d94c13dc8c52f2c65927ba6734391
wahidsaimon <mirajsaimon123@gmail.com>
2024-09-26 00:22:31 +0600

    logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -32,7 +32,7 @@ const Nav = () => {
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">LI SERVICE 24</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -50,7 +50,7 @@ const Nav = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Get started</a>
                 </div>
             </div>
         </div>
