7252eb7652b482a1ea825a8ef0f3bb2aaf164f92
imtiazimran <imtiazimran@outlook.com>
2025-03-29 13:10:07 +0600

    add WhatsApp widget to the app and style it for better visibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav";
 
 const App = () => {
-    return (
-        <div>
-          <Nav />
-          <div className="">
-          <Outlet/>
-          </div>
-            
+  return (
+    <div>
+      <Nav />
+      <div className="">
+        <div id="whatsapp-widget">
+          <a href="https://api.whatsapp.com/message/FI3L5HOJSGYBA1" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
+          </a>
         </div>
-    );
+
+       
+
+        <Outlet />
+      </div>
+
+    </div>
+  );
 };
 
 export default App;
